4945a1db53b9a5cee4956406f7eab00bc468c644
michael <scienceopen@users.noreply.github.com>
2015-11-11 19:13:06 -0500

    skeleton

---
+++ test/test.py
@@ -0,0 +1,13 @@
+from __future__ import division,absolute_import
+from isrutils.snrpower import (readpower_samples,plotsnr,readsnr_int,snrvtime_fit,
+                               plotsnr1d,plotsnrmesh)
+from isrutils.rawacf import readACF
+
+def test_readpowersnr():
+    pass
+
+def test_readacf():
+    pass
+
+def test_readplasma():
+    pass
