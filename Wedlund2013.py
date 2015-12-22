9005d2d8183e9374049816c496b4fdbd8ede8072
scienceopen <scienceopen@users.noreply.github.com>
2015-12-22 11:14:13 -0600

    Ne fwd

--- Wedlund2013.py
+++ Wedlund2013.py
@@ -3,15 +3,19 @@
 given energy deposition matrix A (based on MSIS/IRI/etc)
 and measured electron number density Ne
 estimate solution for q=A Phi
-assuming quasi-stationary system such that
+assuming quasi-stationary system such that production rate
 q = alpha * Ne^2
+where alpha is the dissociative recombination rate assuming Ni~Ne
 """
 from pathlib import Path
 import h5py
 from pandas import DataFrame
-from numpy import atleast_1d,empty_like
-from matplotlib.pyplot import figure,show
+from numpy import empty_like
+from matplotlib.pyplot import figure,show,subplots
+import seaborn as sns
+sns.set_context('talk',font_scale=1.5)
 #
+from gridaurora.eFluxGen import maxwellian
 from gridaurora.chapman import chapman_profile
 
 
@@ -26,13 +30,14 @@ def isrdep(depfn):
 
     return A,zkm,Ek
 
-def isrNeSim(NeFwd,Te):
+def isrNe2q(Fwd):
     """
     From Wedlund 2013 Par. 40, Sheedhan and St.-Maurice 2004
     """
-    hotind = Te>1200.
-    alphaO2 = empty_like(Te,dtype=float)
-    alphaNO = empty_like(Te,dtype=float)
+    Te = Fwd['Te']
+    hotind = Te.values>1200.
+    alphaO2 = empty_like(hotind,dtype=float)
+    alphaNO = empty_like(hotind,dtype=float)
 
     alphaO2[hotind] = 1.95e-7 * (300./Te[hotind])** 0.56  # Te>1200K
     alphaO2[~hotind]= 1.95e-7 * (300./Te[~hotind])**0.70
@@ -41,45 +46,58 @@ def isrNeSim(NeFwd,Te):
     alphaNO[~hotind]= 3.50e-7 * (300./Te[~hotind])**0.69
 
     #NOTE: valid 100-250km altitude Wedlund 2013 Par. 42
-    alpha = (0.478*alphaNO + 0.373*alphaO2)
+    alpha = (0.478*alphaNO + 0.373*alphaO2) #[cm^3 s^-1]
+#%% now invoke quasi-static q=alpha*Ne**2
+    q = alpha * Fwd['Ne']**2
+    return q
 
+def phantom(Z0,H,zkm):
+    Fwd = DataFrame(index=zkm,columns=['Ne','Te'])
+    Fwd['Ne'] = 1e5 * chapman_profile(p.nez0,zkm,p.H) #[electrons cm^-3]
+    # yes this is a repurposing of the maxwellian normally used for energy
+    Fwd['Te'] = maxwellian(zkm,220,2e9)[0]
+    return Fwd
 
+def doplot(Fwd,q,zlim):
+    fg,axs = subplots(2,2,sharey=True)
+#%% electron number density
+    ax = axs[0,0]
+    ax.plot(Fwd['Ne'].values,Fwd.index)
+    ax.set_title('electron number density')
+    ax.set_ylabel('altitude [km]')
+    ax.set_xlabel('electron number density [e$^-$ cm$^{-3}$]')
+#%% Electron temperature
+    ax = axs[0,1]
+    ax.plot(Fwd['Te'].values,Fwd.index)
+    ax.set_title('Electron Temperature')
+    ax.set_xlabel('Tempature [K]')
+#%% production
+    ax= axs[1,0]
+    ax.plot(q.values,q.index)
+    ax.set_title('Production')
+    ax.set_ylabel('altitude [km]')
+    ax.set_xlabel('ionization rate [cm$^{-3}$ s$^{-1}$]')
+
+    [a.set_ylim(zlim) for a in axs.ravel()]
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
-    p = ArgumentParser(descripton='simple demo of solving for flux given ISR Ne measurement')
+    p = ArgumentParser(description='simple demo of solving for flux given ISR Ne measurement')
     p.add_argument('--depfn',help='energy deposition data HDF5 file',default='precompute/transcareigen.h5')
     p.add_argument('--nez0',help='SIMULATION peak ionization altitude [km]',default=110.,type=float)
-    p.add_argument('--H',help='SIMULATION scale height [km]',default=20.,type=float)
+    p.add_argument('--H',help='SIMULATION scale height [km]',default=15.,type=float)
+    p.add_argument('--zlim',help='plotting, altitude limits [km]',default=(90,300),type=float)
     p = p.parse_args()
 
-    A,zkm,Ek = isrdep(p.depfn)
+    A,zkm,E = isrdep(p.depfn)
 
-    NeFWD = DataFrame(data=chapman_profile(p.nez0,zkm,p.H),
-                      index=zkm)
+    Fwd = phantom(p.nez0,p.H,zkm)
 
-    Te = atleast_1d(2000.) #TODO this should be per altitude in real measurements and sims
+    q = isrNe2q(Fwd)
 
-    isrNe2q(NeFWD,Te)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#%% plot
+    doplot(Fwd,q,p.zlim)
+    show()
 
 
 #%% notes
