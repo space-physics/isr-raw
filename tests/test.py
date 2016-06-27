15f89c409174d733b84ef3414733f431c171da3e
scienceopen <scienceopen@users.noreply.github.com>
2016-06-27 15:02:22 -0400

    extract test file properly

--- tests/test.py
+++ tests/test.py
@@ -9,10 +9,12 @@ from isrutils.rawacf import readACF
 from isrutils.plasmaline import readplasmaline
 
 rdir = Path(__file__).parents[1]
-fn = rdir/'tests/test.dt3.h5'
+name = 'test.dt3.h5'
+path = rdir/'tests'
+fn = path/name
 if not fn.is_file():
-    with tarfile.open(str(fn) + '.bz2','r') as f:
-        f.extractall('test.dt3.h5')
+    with tarfile.open(str(fn) + '.xz','r') as f:
+        f.extract(name,str(path))
 
 
 bid=64157 #magnetic zenith beam id for PFISR for experiments I know of  ~ 2011-2016
