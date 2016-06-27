cb0280ca5521bd49fec1871b4bdcf9a23b0cecfa
scienceopen <scienceopen@users.noreply.github.com>
2016-06-27 18:31:26 -0400

    init

---
+++ Examples/PLburst2013Apr14T854.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""
+For a particular event, shows the plasma line bursts
+
+"""
+from isrutils import Path
+from tempfile import gettempdir
+from datetime import datetime
+from pytz import UTC
+# github.com/scienceopen/isrutils
+from isrutils.plasmaline import readplasmaline,plotplasmaline
+
+stem = Path('~/data/2013-04-14/ISR/d0346834').expanduser()
+vlim = (70,100)
+tlim = (datetime(2013,4,14,8,54,10,tzinfo=UTC),
+        datetime(2013,4,14,8,54,50,tzinfo=UTC))
+zlim = (None,None)
+odir = Path(gettempdir()) / 'plplot'
+
+beamid = 64157
+
+spec,freq = readplasmaline(stem,beamid,tlim)
+plotplasmaline(spec,freq,stem,tlim,vlim=vlim,zlim=zlim,makeplot=['png'],odir=odir)
