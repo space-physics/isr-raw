67ee1dace2fa5b100ec9d2f0866f15f478b37a3d
scienceopen <scienceopen@users.noreply.github.com>
2016-06-28 12:52:34 -0400

    simpler, dedupe API

--- Examples/SimpleSNRplot-2013-04-14T0854.py
+++ Examples/SimpleSNRplot-2013-04-14T0854.py
@@ -3,17 +3,14 @@
 2013-04-14 08:54 UT event
 """
 from isrutils import Path
-from tempfile import gettempdir
 from datetime import datetime
 from pytz import UTC
 from matplotlib.pyplot import show
 import seaborn as sns
 sns.set_context('talk',1.5)
 #
-from isrutils.switchyard import isrselect
-from isrutils.plots import plotsnr
+from isrutils.looper import simpleloop
 #%%
-path = Path('~/data/2013-04-14/ISR')
 zlim=(90, 400)
 tlim=(datetime(2013,4,14,8,54,10,tzinfo=UTC),
       datetime(2013,4,14,8,54,50,tzinfo=UTC))
@@ -21,31 +18,22 @@ tlim=(datetime(2013,4,14,8,54,10,tzinfo=UTC),
 tlim2=(datetime(2013,4,14,8,tzinfo=UTC),
       datetime(2013,4,14,9,tzinfo=UTC))
 
-t0 = None#datetime(2013,4,14,8,54,tzinfo=UTC)
+P={'path':Path('~/data/2013-04-14/ISR'),
+   'beamid': 64157,
+   't0':None, #datetime(2013,4,14,8,54,tzinfo=UTC)
+   'showacf':False,
+   'showsamples':True,
+   'makeplot': ('show')
+  }
+#%% iterate over list. Files are ID'd by file extension (See README.rst)
+flist = ('d0346834.dt3.h5', #long pulse
+         'd0346834.dt0.h5') #alt code
 
-odir = gettempdir()
+simpleloop(flist,tlim,zlim,P)
+#%% 30 sec integration
+flist = ('20130413.001_ac_30sec.h5',
+         '20130413.001_lp_30sec.h5')
 
-beamid = 64157
-vlim=None
-showacf=False
-showsamples=True
-makeplot=('show')
-
-#%% long pulse 234ms and 12ms power plots
-isrfn = path/'d0346834.dt3.h5'
-spec,freq,snrsamp,azel,isrlla,snrint,snr30int = isrselect(
-        isrfn,beamid,tlim,zlim,t0,showacf,showsamples)
-plotsnr(snrint,isrfn,tlim)
-#%% alternating code 234ms and 12ms power plots
-isrfn = path/'d0346834.dt0.h5'
-spec,freq,snrsamp,azel,isrlla,snrint,snr30int = isrselect(
-        isrfn,beamid,tlim,zlim,t0,showacf,showsamples)
-plotsnr(snrint,isrfn,tlim)
-#%% alt code 30 sec
-#spec,freq,snrsamp,azel,isrlla,snrint,snr30int = isrselect(
-#        path/'20130413.001_ac_30sec.h5',beamid,tlim2,zlim,t0,showacf,showsamples)
-# long pulse 30 sec
-#spec,freq,snrsamp,azel,isrlla,snrint,snr30int = isrselect(
-#        path/'20130413.001_lp_30sec.h5',beamid,tlim2,zlim,t0,showacf,showsamples)
+simpleloop(flist,tlim2,zlim,P)
 
 show()
