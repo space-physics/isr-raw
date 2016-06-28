6d725fd1720c0190853509caeef50486efda2e83
scienceopen <scienceopen@users.noreply.github.com>
2016-06-28 11:48:07 -0400

    update path structure

--- Examples/JointISRHST_T854.py
+++ Examples/JointISRHST_T854.py
@@ -1,27 +1,26 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 """
 plots integrated ISR power over altitude range on top of HiST image stream
-
-Michael Hirsch
 """
+from isrutils import Path
 from collections import namedtuple
 from matplotlib.pyplot import show
 from pytz import UTC
 from datetime import datetime
-from tempfile import gettempdir
+from tempdir import gettempdir
 #
 from isrutils.overlayISRopt import overlayisrhist
 
-isrfn='~/Dropbox/aurora_data/StudyEvents/2013-04-14/ISR/d0346834.dt3.h5'
+isrfn='~/data/2013-04-14/ISR/d0346834.dt3.h5'
 zlim=(200, 350)
 tlim=(datetime(2013,4,14,8,54,10,tzinfo=UTC),
       datetime(2013,4,14,8,54,50,tzinfo=UTC))
-odir = gettempdir()
+odir = Path(gettempdir()) / (str(tlim[0])+'.mkv')
 
 P = namedtuple('a',['azelfn','optfn','makeplot'])
 
 P.azelfn='~/code/histfeas/precompute/hst0cal.h5'
-P.optfn ='~/Dropbox/aurora_data/StudyEvents/2013-04-14/HST/2013-04-14T8-54_hst0.h5'
+P.optfn ='~/data/2013-04-14/HST/2013-04-14T8-54_hst0.h5'
 P.makeplot=('png')
 P.beamid = 64157
 
