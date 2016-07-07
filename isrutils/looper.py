463f2224caa4f0a9731775babbef484e53cd48f0
scienceopen <scienceopen@users.noreply.github.com>
2016-07-07 14:04:03 -0400

    smarter API and plot limits

--- isrutils/looper.py
+++ isrutils/looper.py
@@ -3,18 +3,14 @@ from . import Path
 from .switchyard import isrselect
 from .plots import plotsnr,plotplasmaline
 
-def simpleloop(flist,tlim,zlim,vlim,P):
+def simpleloop(flist,P):
+
+    tlim,vlim = P['tlim'],P['vlim']
 
     path = Path(P['path'])
-    try:
-        t0 = P['t0']
-    except KeyError:
-        t0 = None
 
     for f in flist:
-        spec,freq,snrsamp,azel,isrlla,snrint,snr30int = isrselect(
-            path/f, P['beamid'], tlim, zlim, t0, P['showacf'],
-            P['showsamples'])
+        spec,freq,snrsamp,azel,isrlla,snrint,snr30int = isrselect(path/f, P['beamid'], P)
         # 15 sec integration
         plotsnr(snrint,f,tlim,vlim)
         # 200 ms integration
