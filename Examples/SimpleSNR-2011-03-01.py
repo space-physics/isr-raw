8787c3af17ef425036b597a65bb2dbf8e9793124
Michael H <nobody@github.com>
2016-07-08 05:02:03 -0400

    acf freq scaling

--- Examples/SimpleSNR-2011-03-01.py
+++ Examples/SimpleSNR-2011-03-01.py
@@ -19,17 +19,14 @@ sns.set_context('talk',1.5)
 #
 from isrutils.looper import simpleloop
 #%% users param
-vlim=(22,55)
-zlim=(90, 400)
-#tlim=(datetime(2011,3,1,10,6,tzinfo=UTC),
-#      datetime(2013,3,1,10,7,tzinfo=UTC))
-tlim=(None,None)
-
-
-P={'path':'~/data/2011-03-01/isr',
+P={'path':'/mnt/c/data/2011-03-01/isr',
    'beamid': 64157,
-   'showacf': True,
-   'showsamples': True,
+   'acf': True,
+   'samples': True,
+   'vlim':  (22,55),
+   'vlimacf': (20,50),
+    'zlim': (90, 400),
+    'tlim': (None,None)
   }
 #%% iterate over list. Files are ID'd by file extension (See README.rst)
 flist = (
@@ -51,6 +48,6 @@ flist = (
 #'d0245972.dt3.h5',
 #'d0245972.dt0.h5'
 )
-simpleloop(flist,tlim,zlim,vlim,P)
+simpleloop(flist,P)
 
 show()
