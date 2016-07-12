1a41ce4f3df7c9627e021d5c9bd0f7eef51e45d5
michael <nobody@noreply.github.com>
2016-07-12 06:14:57 -0400

    better default

--- Examples/SimpleSNR-2013-04-14T0854.py
+++ Examples/SimpleSNR-2013-04-14T0854.py
@@ -15,19 +15,19 @@ from isrutils.looper import simpleloop
 P={'path':'~/data/2013-04-14/isr',
    'makeplot': [],
    'beamid': 64157,
-   'acf': True,
+   'acf': False,
    'vlimacf': (20,50),
    'samples': True,
    'odir': 'out/try',
    'vlim': (22,55),
    'zlim': (90, 400),
-   'tlim': (datetime(2013,4,14,8,54,20,tzinfo=UTC),
-            datetime(2013,4,14,8,54,21,tzinfo=UTC)),
+   'tlim': (datetime(2013,4,14,8,54,10,tzinfo=UTC),
+            datetime(2013,4,14,8,54,50,tzinfo=UTC)),
   }
 #%% iterate over list. Files are ID'd by file extension (See README.rst)
 flist = (
 'd0346834.dt3.h5', #long pulse
-'d0346834.dt0.h5', #alt code
+#'d0346834.dt0.h5', #alt code
 #'20130413.001_ac_30sec.h5',
 #'20130413.001_lp_30sec.h5'
 )
