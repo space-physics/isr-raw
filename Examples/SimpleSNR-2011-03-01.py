071c96b35ce672bbc253ed18351d5c79eae3e35b
scienceopen <scienceopen@users.noreply.github.com>
2016-07-13 00:26:47 -0400

    exmaple api

--- Examples/SimpleSNR-2011-03-01.py
+++ Examples/SimpleSNR-2011-03-01.py
@@ -19,14 +19,14 @@ sns.set_context('talk',1.5)
 #
 from isrutils.looper import simpleloop
 #%% users param
-P={'path':'/mnt/c/data/2011-03-01/isr',
+P={'path':'~/data/2011-03-01/isr',
    'beamid': 64157,
    'acf': True,
    'samples': True,
    'vlim':  (22,55),
    'vlimacf': (20,50),
     'zlim': (90, 400),
-    'tlim': (None,None)
+    'tlim': (None,datetime(2011,3,1,10,6,tzinfo=UTC))
   }
 #%% iterate over list. Files are ID'd by file extension (See README.rst)
 flist = (
@@ -34,7 +34,7 @@ flist = (
 #'d0245965.dt0.h5',
 'd0245966.dt3.h5',
 #'d0245966.dt2.h5',
-'d0245966.dt0.h5',
+#'d0245966.dt0.h5',
 #'d0245967.dt3.h5',
 #'d0245967.dt0.h5',
 #'d0245968.dt3.h5',
