e382b75044a6bc6553443846970404b84dee33a3
michael <nobody@noreply.github.com>
2016-07-14 03:26:17 -0400

    stubbing in raw ACF computation, auto integrated SNR vlim

--- isrutils/rawacf.py
+++ isrutils/rawacf.py
@@ -1,26 +1,31 @@
 from __future__ import division
 from six import integer_types
 from . import Path
-import logging
 import h5py
+import logging
 from xarray import DataArray
 from numpy import (empty,zeros,complex128,conj,append,linspace,column_stack)
+from numpy import correlate as xcorr
 from numpy.fft import fft,fftshift
 #
 from .common import ftype,ut2dt,findstride
 from .plots import plotacf
+from .snrpower import filekey
 
 def compacf(acfall,noiseall,Nr,dns):
     """
     acf all:  Nlag x Nslantrange x real/comp
 
     """
-    assert acfall.ndim == 3
+    assert acfall.ndim in (3,2)
 
     Nlag = acfall.shape[0]
     spec = empty((Nr,2*Nlag-1),complex128)
 
-    acf = (acfall[...,0] + 1j*acfall[...,1]).T / dns / 2.
+    if acfall.ndim == 3: # last dim real,cplx
+        acf = (acfall[...,0] + 1j*acfall[...,1]).T / dns / 2.
+    elif acfall.ndim == 2:
+        acf = acfall / dns / 2.
 
     try:
         acf_noise = (noiseall[...,0] + 1j*noiseall[...,1]).T / dns / 2.
@@ -54,41 +59,53 @@ def readACF(fn,P):
         t = ut2dt(f['/Time/UnixTime'].value)
 
         ft = ftype(fn)
+        noisekey = None
+#%%
         if ft == 'dt3':
             rk = '/S/'
             try:
-                noiseall = f[rk+'Noise/Acf/Data']
+                acfkey = f[rk+'Data/Acf/Data']
+                noisekey = f[rk+'Noise/Acf/Data']
             except KeyError:
-                logging.warning('{} does not exist in {}'.format(rk,fn))
-                return
+                acfkey = f[filekey(f)+'/Samples/Data']
+#%%
         elif ft == 'dt0':
             rk = '/IncohCodeFl/'
-            noiseall = None #TODO hack for dt0
+            acfkey = f[rk+'Data/Acf/Data']
         else:
             raise TypeError('unexpected file type {}'.format(ft))
+#%%
+        try:
+            srng = f[rk + 'Data/Acf/Range']
+            bstride = findstride(f[rk+'Data/Beamcodes'],P['beamid'])
+        except KeyError: # old 2007 files
+            srng = f[filekey(f)+'/Power/Range']
+            bstride = findstride(f['/RadacHeader/BeamCode'],P['beamid'])
 
-        if not rk in f:
-           logging.warning('{} does not exist in {}'.format(rk,fn))
-           return
-
-        srng = f[rk + 'Data/Acf/Range'].value.squeeze()
-        bstride = findstride(f[rk+'Data/Beamcodes'],P['beamid'])
-#        bcodemap = DataArray(data=f['/Setup/BeamcodeMap'][:,1:3],
-#                             dims=['beamcode','azel'],
-#                             coords={'beamcode':f['/Setup/BeamcodeMap'][:,0].astype(int),
- #                                    'azel':['az','el']}
-#                            )
         i = (P['beamid'] == f['/Setup/BeamcodeMap'][:,0]).nonzero()[0].item()
         azel = f['/Setup/BeamcodeMap'][i,1:3]
 
         istride = column_stack(bstride.nonzero())
         for tt,s in zip(t,istride):
-            spectrum,acf = compacf(f[rk+'Data/Acf/Data'][s[0],s[1],:,:,:],
-                                   noiseall[s[0],s[1],:,:,:],
+            if noisekey is not None:
+                spectrum,acf = compacf(acfkey[s[0],s[1],...],
+                                   noisekey[s[0],s[1],...],
                                    srng.size, dns)
+            elif acfkey.ndim==5:
+                spectrum,acf = compacf(acfkey[s[0],s[1],...],
+                                   noisekey,
+                                   srng.size, dns)
+            elif acfkey.ndim==4: #raw samples from 2007 file
+                logging.critical('TODO this code not complete--need to have all the lags as a dimension')
+                tdat = acfkey[s[0],s[1],:,0] + 1j*acfkey[s[0],s[1],:,1]
+                acfall = xcorr(tdat, tdat, 'full')
+                spectrum,acf = compacf(acfall,
+                                   noisekey,
+                                   srng.size, dns)
+
             specdf = DataArray(data=spectrum,
                                dims=['srng','freq'],
-                               coords={'srng':srng,
+                               coords={'srng':srng.value.squeeze(),
                                        'freq':linspace(-freqscalefact,freqscalefact,spectrum.shape[1])})
             try:
                 plotacf(specdf,fn,azel,tt, P, ctxt='dB')
