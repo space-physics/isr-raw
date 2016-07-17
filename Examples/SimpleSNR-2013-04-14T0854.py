00944120b6c592ad605cc8f09669e28b0edbcf4e
scienceopen <scienceopen@users.noreply.github.com>
2016-07-17 15:29:42 -0400

    api example [ci skip]

--- Examples/SimpleSNR-2013-04-14T0854.py
+++ Examples/SimpleSNR-2013-04-14T0854.py
@@ -7,6 +7,7 @@ from pytz import UTC
 from matplotlib.pyplot import show
 import seaborn as sns
 sns.set_context('talk',1.5)
+sns.set_style('ticks')
 #
 from isrutils.looper import simpleloop
 #%% users param
@@ -15,22 +16,25 @@ from isrutils.looper import simpleloop
 P={'path':'~/data/2013-04-14/isr',
    'makeplot': [],
    'beamid': 64157,
-   'acf': True,
+   'acf': False,
    'vlimacf': (20,50),
-   'zlim_pl': (None,None),
-   'vlim_pl': (None,None),
+   'zlim_pl': [None,None],
+   'vlim_pl': [None,None],
+   'flim_pl': [None,None],
+   'int': False,
    'samples': True,
-   'odir': '',
-   'vlim': (22,55),
-   'zlim': (90, 400),
+   'odir': 'out/2013-04-14',
+   'vlim': [22,55],
+   'zlim': (90, None),
    'tlim': (datetime(2013,4,14,8,54,10,tzinfo=UTC),
             datetime(2013,4,14,8,54,50,tzinfo=UTC)),
   }
 #%% iterate over list. Files are ID'd by file extension (See README.rst)
 flist = (
-'d0346834.dt3.h5', #long pulse
-#'d0346834.dt0.h5', #alt code
-#'d0346834.dt1.h5',
+'d0346834.dt3.h5', # 480 us long pulse
+'d0346834.dt1.h5',
+'d0346834.dt0.h5', #alt code
+
 #'20130413.001_ac_30sec.h5',
 #'20130413.001_lp_30sec.h5'
 )
