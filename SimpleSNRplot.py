b1f701e65a2a587f25aded2043920bf9634039a7
scienceopen <scienceopen@users.noreply.github.com>
2016-08-07 03:27:21 -0400

    new api

--- SimpleSNRplot.py
+++ SimpleSNRplot.py
@@ -4,13 +4,25 @@ reading PFISR data down to IQ samples
 
 See Examples/ for more updated specific code
 """
-#
+from isrutils import Path
 from isrutils.common import boilerplateapi
-from isrutils.switchyard import isrselect
-from isrutils.plots import plotsnr
-#%% read data
-p,isrfn,odir,tlim = boilerplateapi()
-spec,freq,snrsamp,azel,isrlla,snrint,snr30int = isrselect(
-                        isrfn,p.beamid,tlim,p.zlim,p.t0,p.acf,p.samples)
-#%% plot data
-plotsnr(snrint,isrfn,p,azel)
+from isrutils.looper import simpleloop
+#%% user parameters
+p = boilerplateapi()
+P={
+'path':p.isrfn,
+'beamid': 64157,
+'acf': True,
+'vlimacf': (18,45),
+'zlim_pl': p.zlim,
+'vlim_pl': [72,90],
+'flim_pl': [3.5,5.5],
+'odir': p.odir,
+'vlim': [25, 55],
+'zlim': p.zlim,
+'tlim': p.tlim,
+'verbose': True,}
+#%% read and plot
+flist = [x for x in Path(P['path']).expanduser().iterdir() if x.suffix=='.h5']
+
+simpleloop(flist,P)
