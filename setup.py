37aa1f7f6f36091e3ed17940d00adc4c4e890477
scienceopen <scienceopen@users.noreply.github.com>
2017-02-05 03:42:10 -0500

    fallcak

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+try:
+    import conda.cli
+    conda.cli.main('install','--file','requirements.txt')
+except Exception as e:
+    print(e)
+    import pip
+    pip.main(['install','-r','requirements.txt'])
+
 setup(name='isrutils',
-	  install_requires=['nose','python-dateutil','pytz','numpy','xarray','matplotlib','seaborn','h5py',
-                        'pathvalidate',
+	  install_requires=['pathvalidate',
                          'histutils','pymap3d','GeoData'],
       dependency_links = ['https://github.com/scienceopen/histutils/tarball/master#egg=histutils',
                           'https://github.com/scienceopen/pymap3d/tarball/master#egg=pymap3d',
