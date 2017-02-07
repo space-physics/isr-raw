841dde2f99b246fedb8c9967c3b18ac0a24eb25d
scienceopen <scienceopen@users.noreply.github.com>
2017-02-07 00:23:07 -0500

    meta

--- setup.py
+++ setup.py
@@ -10,10 +10,10 @@ except Exception as e:
     pip.main(['install','-r','requirements.txt'])
 
 setup(name='isrutils',
+      author='Michael Hirsch, Ph.D.',
 	  install_requires=['pathvalidate',
                          'histutils','pymap3d','GeoData'],
       dependency_links = ['https://github.com/scienceopen/histutils/tarball/master#egg=histutils',
-                          'https://github.com/scienceopen/pymap3d/tarball/master#egg=pymap3d',
                           'https://github.com/jswoboda/GeoDataPython/tarball/master#egg=GeoData',],
       packages=['isrutils'],
 	  )
