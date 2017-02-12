0a5f28ae803bb9bfa53ef5df3b711b2e9f58ee2c
michael <hirsch617@gmail.com>
2017-02-12 03:42:00 -0500

    prereq

--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(name='isrutils',
       author='Michael Hirsch, Ph.D.',
       url='https://github.com/scienceopen/isrutils',
       description='utilities for reading and plotting ISR raw data',
+      version='0.5',
 	  install_requires=req,
       dependency_links = [
-            'https://github.com/jswoboda/GeoDataPython/tarball/master#egg=GeoData',],
+            'https://github.com/jswoboda/GeoDataPython/tarball/master#egg=GeoData-999.0.0',],
 	  )
