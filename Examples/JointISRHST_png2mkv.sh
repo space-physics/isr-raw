97e0c7fe9b1ad51b647f38a7747644f4efc9b40c
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-05-24 15:45:54 -0400

    doc [ci skip]

--- Examples/JointISRHST_png2mkv.sh
+++ Examples/JointISRHST_png2mkv.sh
@@ -3,7 +3,7 @@
 
 cd /tmp/20*
 
-ren.sh "*.png"   # scienceopen/pybashutils
+ren.sh "*.png"   # scivision/pybashutils
 
 ffmpeg -framerate 5 -pattern_type glob -i "*.png" -c:v ffv1 ~/Dropbox/aurora_data/StudyEvents/2013-04-14/out.mkv
 
